6daa673c4dd8c61909819f5b9491e22ed9186c73
manders <manders@teleworx.com>
2018-05-01 14:40:16 -0400

    noUI

---
+++ src/teleworx/worldwind/WorldWindAPITest.java
@@ -0,0 +1,14 @@
+package teleworx.worldwind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorldWindAPITest {
+
+	@Test
+	public void testJunitTest1() {
+		assertEquals(1, WorldWindAPI.junitTest1());
+	}
+
+}
